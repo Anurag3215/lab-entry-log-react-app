be12c9e9597c37e6230a183cd9dd5ae3bf307c33
Anurag3215 <anuragaacc@gmail.com>
2026-07-14 14:43:56 +0530

    Add Log Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddLog from './components/AddLog'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +11,8 @@ function App() {
   return (
     <>
       
+    <AddLog/>
 
-      
     </>
   )
 }
